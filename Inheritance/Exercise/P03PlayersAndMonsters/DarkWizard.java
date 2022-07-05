1744c206c0de5dffe8010d3d9c94e7bf1373574f
MartinKrystev <martin.krystev@gmail.com>
2022-07-05 20:26:31 +0300

    PlayersAndMonsters

---
+++ Inheritance/Exercise/P03PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package com.company.Inheritance.Exercise.P03PlayersAndMonsters;
+
+public class DarkWizard extends Wizard {
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
---
+++ Inheritance/Exercise/P03PlayersAndMonsters/Main.java
@@ -0,0 +1,9 @@
+package com.company.Inheritance.Exercise.P03PlayersAndMonsters;
+
+public class Main {
+    public static void main(String[] args) {
+        BladeKnight bladeKnight = new BladeKnight("Pesho", 3);
+
+        System.out.println(bladeKnight);
+    }
+}
