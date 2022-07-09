9201b74b4585812a20f5d73776f14b47aea425ab
MartinKrystev <martin.krystev@gmail.com>
2022-07-09 19:27:56 +0300

    Animals

---
+++ Polymorphism/Lab/P03Animals/Dog.java
@@ -0,0 +1,13 @@
+package com.company.Polymorphism.Lab.P03Animals;
+
+public class Dog extends Animal {
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("%s%nDJAAF", super.explainSelf());
+    }
+}
